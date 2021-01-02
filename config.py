a2bca58ae6e40b98a83d0f0c61c417b3e340b090
waterBottleHolster <kirk-mcmillan@hotmail.com>
2021-01-01 19:51:41 -0800

    The fourth pair of login/registration pages has a big wood colored background, I stole it from a woodworking template online

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SEND_FILE_MAX_AGE_DEFAULT = False
 
     @staticmethod
     def init_app(app):
