b2960670237dec43e69edb460f4f89a3e36f5ab2
waterBottleHolster <kirk-mcmillan@hotmail.com>
2021-01-04 19:14:03 -0800

    added bootstrap icons directory and made partial edits to login7.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
                 next = url_for('main.index')
             return redirect(next)
         flash('Invalid Username or Password')
-    return render_template('auth/login6.html', form=form)
+    return render_template('auth/login7.html', form=form)
 
 @auth.route('/logout')
 @login_required
@@ -37,4 +37,4 @@ def register():
         db.session.commit()
         flash('You can now login!')
         return redirect(url_for('auth.login'))
-    return render_template('auth/register6.html', form=form)
+    return render_template('auth/register7.html', form=form)
